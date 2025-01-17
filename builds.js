0638caed368ad9b597530ff5670119aad7574563
caiwenxi <632836963@qq.com>
2025-01-17 17:43:02 +0800

    1

--- builds.js
+++ builds.js
@@ -3,11 +3,20 @@ import { build } from "vite"
 const config1 = {
   define:{
     _APP_COLOR_: JSON.stringify('default'),//颜色
-    _APP_HISTORY_: 2,
+    _APP_HISTORY_: JSON.stringify('1'),//颜色
+  },
+  build: {
+    outDir: 'dist', //打包后文件名
+  },
+}
+const config2 = {
+  define:{
+    _APP_COLOR_: JSON.stringify('default'),//颜色
+    _APP_HISTORY_: JSON.stringify('2'),//颜色
   },
   build: {
     outDir: 'dist1', //打包后文件名
   },
 }
-
 await build(config1)
+await build(config2)
